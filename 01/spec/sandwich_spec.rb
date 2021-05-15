696b74a2913241d6c049111b08d48a2d841e1bf7
SneyderHOL <1507@holbertonschool.com>
2021-05-14 23:00:30 -0500

    Sharing setup

--- 01/spec/sandwich_spec.rb
+++ 01/spec/sandwich_spec.rb
@@ -1,3 +1,4 @@
+# Using let construct - initialize data on demand
 Sandwich = Struct.new(:taste, :toppings)
 RSpec.describe 'An ideal sandwich' do
   let(:sandwich) { Sandwich.new('delicious', []) }
@@ -15,3 +16,70 @@ RSpec.describe 'An ideal sandwich' do
     expect(toppings).not_to be_empty
   end
 end
+
+=begin
+# Using helper methods, specify when to run
+Sandwich = Struct.new(:taste, :toppings)
+RSpec.describe 'An ideal sandwich' do
+  def sandwich
+    @sandwich ||= Sandwich.new('delicious', [])
+  end
+  
+  it 'is delicious' do
+    taste = sandwich.taste
+
+    expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
+  end
+end
+=end
+
+=begin
+# Using struct
+Sandwich = Struct.new(:taste, :toppings)
+RSpec.describe 'An ideal sandwich' do
+  it 'is delicious' do
+    sandwich = Sandwich.new('delicious', [])
+    
+    taste = sandwich.taste
+
+    expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do
+    sandwich = Sandwich.new('delicious', [])
+    
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
+  end
+end
+=end
+
+=begin
+# Using hooks, run automatically at specific times during testing
+Sandwich = Struct.new(:taste, :toppings)
+RSpec.describe 'An ideal sandwich' do
+  before { @sandwich = Sandwich.new('delicious', []) }
+  
+  it 'is delicious' do
+    taste = @sandwich.taste
+
+    expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do
+    @sandwich.toppings << 'cheese'
+    toppings = @sandwich.toppings
+
+    expect(toppings).not_to be_empty
+  end
+end
+=end
